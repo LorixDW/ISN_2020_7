19979e510799765d0dc6f4f417cfb41352ad7b41
LorixDW <pizhyanov.slava@gmail.com>
2023-02-28 03:03:16 +0300

    v1.0 realize

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: 'public/',
 	globPatterns: [
-		'**/*.{ico,html,json}'
+		'**/*.{png,ico,html,json,js}'
 	],
 	swDest: 'public/sw.js',
 	ignoreURLParametersMatching: [
