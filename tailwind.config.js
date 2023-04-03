294fae80bff68564f0e0c082e5e99d412112a901
Lorix <pizhyanov.slava@yandex.ru>
2023-04-04 02:11:38 +0300

    v1.0 realize

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true
+    },
     extend: {},
+    screens: {
+      'xl': '1280px',
+      'lg': '1024px',
+      'md': '768px',
+      'sm': '375px'
+    }
   },
   plugins: [],
 }
