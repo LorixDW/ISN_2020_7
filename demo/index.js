aa84ef255d842f427304ba7a7eb1304d5f4adcb1
Lorix <pizhyanov.slava@gmail.com>
2023-05-02 11:21:02 +0300

    v1.0 - ready

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,63 @@
-import { saveVideoLocal } from './node_modules/video_saving_service/vidio_saving.js'
+let btn = document.getElementById("send")
+let reader = new FileReader()
+VideoListUpdate()
 
-document.getElementById('send').onclick(SendVideo())
-
-function SendVideo(){
+function Send(){
     let file = document.getElementById('file').files[0]
-    saveVideoLocal(file, 'base')
+    reader.readAsDataURL(file)
+    reader.onload = function () {
+        localStorage.setItem('video', reader.result);
+        localStorage.setItem('name', file.name)
+    }
+    setTimeout(VideoListUpdate, 400)
 }
+
+//one video
+function VideoListUpdate() {
+    let list = document.querySelector('#video')
+    let player = document.querySelector('#view')
+    console.log(localStorage.key(1))
+    list.innerHTML = '';
+    player.innerHTML = '';
+    if(localStorage.key(1) !== null){
+        let p = document.createElement("p")
+        p.innerText = localStorage.getItem(localStorage.key(0))
+        let view = document.createElement("button")
+        view.innerText = "View"
+        view.addEventListener("click", function(e){
+            player.innerHTML = ""
+            let v = document.createElement("video")
+            v.controls = true;
+            v.src = localStorage.getItem("video")
+            v.width = 400
+            player.append(v)
+        })
+        let del = document.createElement("button")
+        del.innerText = "Delete"
+        del.addEventListener("click", function(e){
+            player.innerHTML = ""
+            localStorage.removeItem("video")
+            VideoListUpdate()
+        })
+        list.append(p, view, del)
+    }
+    
+}
+
+//many videos
+// function VideoListUpdate() {
+//     let list = document.querySelector('#list')
+//     list.innerHTML = ''
+//     let list_ = []
+//     for(let i = 0; i < localStorage.length; i += 1){
+//         let p = document.createElement("p")
+//         let view = document.createElement("button")
+//         let del = document.createElement("button")
+//         let li = document.createElement("li")
+//         p.innerText = localStorage.key(i)
+//         view.innerText = "View"
+//         del.innerText = "Delete"
+//         li.append(p, view, del)
+//         list.appendChild(li)
+//     }
+// }
